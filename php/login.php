1a8b1460fdeef56be7aa425c406cd578d1d972e5
Akeiten <Akeiten@gmail.com>
2015-02-03 19:28:02 +0000

    updated authentication

--- php/login.php
+++ php/login.php
@@ -4,7 +4,7 @@
     ini_set("display_errors",1);
     ini_set("display_startup_errors",1);
     error_reporting(E_ALL & ~E_NOTICE);
-    //nclude 'include/config.php';
+    include 'include/config.php';
   
   function result($success, $message) {
     $response['success'] = $success;
@@ -12,5 +12,45 @@
     echo json_encode($response);
   }
   
-  result(true, "hi tan");
+class Login
+{
+
+	public function __construct($email, $password)
+	{
+		//echo "Haxxor";
+		$this->email = $email;
+		$this->password = $password;
+		
+	}
+	
+	private function checkInputs()
+	{
+		return true;
+	}
+	
+	private function authenticate()
+	{
+	    $this->conn = connectDB();
+	    
+		$stmt = $this->conn->prepare("SELECT * FROM Users WHERE email = ?");
+		$stmt->bindValue(1, $this->email);
+		$stmt->execute();		
+		$registrants = $stmt->fetchAll();
+		
+		if(count($registrants) == 0) 
+		{
+			result(false, "User already exists");
+			return;
+		}
+		
+		echo json_encode($registrants);
+		
+		
+	}
+	
+	
+}
+
+$login = new Login('zoolander@mayfair.com', 'abc123');
+$login->authenticate();
 ?>
