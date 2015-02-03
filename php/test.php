79ddcbc8f34f6c9563a9fdad0014035d67afa46a
Akeiten <Akeiten@gmail.com>
2015-02-03 18:21:21 +0000

    Removed echos

--- php/test.php
+++ php/test.php
@@ -5,13 +5,7 @@
     ini_set("display_errors",1);
     ini_set("display_startup_errors",1);
     error_reporting(E_ALL & ~E_NOTICE);
-
-    echo "step1";
     include 'include/config.php';
-    
-    echo "step2";
-
-    $conn = connectDB();
-    echo "step3";
+    //$conn = connectDB();
 
 ?>
