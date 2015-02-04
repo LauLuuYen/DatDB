3d9276f502feaf64aab9ade7041b3f7d696f6543
Akeiten <Akeiten@gmail.com>
2015-02-04 19:48:51 +0000

    changed uploadfile log

--- js/app.js
+++ js/app.js
@@ -81,7 +81,7 @@ app.controller('Home', function ($scope, master) {
                //Check file size 
                var byte = $("#uploadfile")[0].files[0].size; //5000000 (5mb)
                alert($("#uploadfile"));
-               console.log($("#uploadfile"));
+               console.log($("#uploadfile")[0]);
                
 		break;
 
