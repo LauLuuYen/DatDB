c690729138e1c7603986da4bf6c8fe00886d08e8
derng <tuan_v_nguyen@hotmail.com>
2015-02-04 20:14:50 +0000

    Update test.php

--- php/test.php
+++ php/test.php
@@ -8,5 +8,36 @@
     include 'include/config.php';
     //$conn = connectDB();
 
-    echo "testing tan";
+if (!empty($_FILES["myFile"])) {
+    $myFile = $_FILES["myFile"];
+ 
+    if ($myFile["error"] !== UPLOAD_ERR_OK) {
+        echo "<p>An error occurred.</p>";
+        exit;
+    }
+ 
+/*
+    // ensure a safe filename
+    $name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
+ 
+    // don't overwrite an existing file
+    $i = 0;
+    $parts = pathinfo($name);
+    while (file_exists(UPLOAD_DIR . $name)) {
+        $i++;
+        $name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
+    }
+ */
+
+    if (!$success) { 
+        echo "<p>Unable to save file.</p>";
+        exit;
+    } else {
+        
+        echo "Uploaddededed!";
+    }
+} else {
+    echo "nothing";   
+}
+}
 ?>
