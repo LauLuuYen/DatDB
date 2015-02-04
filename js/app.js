150038557d1f506516e0a724201b7bbc8c8a4813
derng <tuan_v_nguyen@hotmail.com>
2015-02-04 19:28:29 +0000

    checking extension

--- js/app.js
+++ js/app.js
@@ -58,8 +58,34 @@ app.controller('Home', function ($scope, master) {
     $scope.send = function() {
     	
     	var filename = $("#uploadfile").val();
-    	
-    	alert("test" + filename);
+        // Use a regular expression to trim everything before final dot
+        var extension = filename.replace(/^.*\./, '');
+
+        // Iff there is no dot anywhere in filename, we would have extension == filename,
+        // so we account for this possibility now
+        if (extension == filename) {
+            extension = '';
+        } else {
+            // if there is an extension, we convert to lower case
+            // (N.B. this conversion will not effect the value of the extension
+            // on the file upload.)
+            extension = extension.toLowerCase();
+        }
+
+        switch (extension) {
+            case 'jpg':
+            case 'jpeg':
+            case 'png':
+                alert("it's got an extension which suggests it's a PNG or JPG image (but N.B. that's only its name, so let's be sure that we, say, check the mime-type server-side!)");
+		break;
+
+            default:
+                // Cancel the form submission
+                alert("wrong extension");
+                submitEvent.preventDefault();
+                
+        }
+        
     }
 
 });
