e2a0eb42d214eda5533b1e6ddeee17155b192550
Tuan <tuan_v_nguyen@hotmail.com>
2015-02-13 16:38:52 +0000

    added mysqli connections

--- php/include/config.php
+++ php/include/config.php
@@ -1,10 +1,11 @@
 <?php
 
-  define('DB_USERNAME', 'ba04b5e16c0e89');
-  define('DB_PASSWORD', '4e6c24f2');
-  define('DB_HOST', 'eu-cdbr-azure-west-b.cloudapp.net');
-  define('DB_NAME', 'lauluuyen');
+    define("DB_USERNAME", "ba04b5e16c0e89");
+    define("DB_PASSWORD", "4e6c24f2");
+    define("DB_HOST", "eu-cdbr-azure-west-b.cloudapp.net");
+    define("DB_NAME", "lauluuyen");
   
+  //TODO remove
   function connectDB() {
       try {
           $conn = new PDO( "mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
@@ -16,5 +17,28 @@
       
       return $conn;
   }
+
+    /*
+    *   Establish connection to database.
+    *   @params: none
+    *   @return: mysqli - $conn
+    */
+    function connectDatabase() {
+        $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if (mysqli_connect_errno()) {
+            die("Failed to connect to database");
+        }
+        return $conn;
+    }
+
+
+    /*
+    *   Close database connection.
+    *   @params: mysqli - $conn
+    *   @return: none
+    */
+    function closeDatabase($conn) {
+        $conn->close();
+    }
 
 ?>
