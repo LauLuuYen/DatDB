b2eac0b57b84cb6311e8d4629225a400aa92c12a
Tuan <tuan_v_nguyen@hotmail.com>
2015-02-15 14:44:42 +0000

    correct datbase string

--- php/include/config.php
+++ php/include/config.php
@@ -4,27 +4,14 @@
     define("DB_PASSWORD", "4e6c24f2");
     define("DB_HOST", "eu-cdbr-azure-west-b.cloudapp.net");
     define("DB_NAME", "lauluuyen");
-  
-  //TODO remove
-  function connectDB() {
-      try {
-          $conn = new PDO( "mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
-          $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-      }
-      catch(Exception $e) {
-          die(var_dump($e));
-      }
-      
-      return $conn;
-  }
 
     /*
     *   Establish connection to database.
     *   @params: none
     *   @return: mysqli - $conn
     */
-    function connectDatabase() {
-        $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    function connectDB() {
+        $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASS, DB_NAME);
         if (mysqli_connect_errno()) {
             die("Failed to connect to database");
         }
@@ -37,7 +24,7 @@
     *   @params: mysqli - $conn
     *   @return: none
     */
-    function closeDatabase($conn) {
+    function closeDB($conn) {
         $conn->close();
     }
 
