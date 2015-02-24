fd69b9fb840408db9a8e5bef052c283726e09191
Tuan <tuan_v_nguyen@hotmail.com>
2015-02-24 16:57:32 +0000

    testing sessions

---
+++ php/session.php
@@ -0,0 +1,52 @@
+<?php
+
+
+class Session {
+
+    /*
+    *   Start session instance
+    *   @params: none
+    *   @return: none
+    */
+	public function __construct() {
+        session_start();
+	}
+	
+
+    /*
+    *   Get the session value at the key.
+    *   @params: none
+    *   @return: ? - $value
+    */
+	public function getSessionVal($key) {
+        $value = $_SESSION["".$key];
+        return $value;
+	}
+	
+
+    /*
+    *   Set the session value at the key.
+    *   @params: string - $key, ? - $value
+    *   @return: none
+    */
+    public function setSessionVal($key, $value) {
+        $_SESSION["".$key] = $value;
+    }
+    
+
+    /*
+    *   Destroy the session.
+    *   @params: none
+    *   @return: none
+    */
+    public function destroySession() {
+        echo "Destroy TODO";
+    }
+    
+}
+
+
+$userSession = new Session();
+
+
+?>
--- php/test.php
+++ php/test.php
@@ -1,5 +1,19 @@
 <?php
 
-    echo "test";
+header("Access-Control-Allow-Origin: *");
+define("DEBUG", true);
+
+if (DEBUG) {
+    ini_set("display_errors",1);
+    ini_set("display_startup_errors",1);
+    error_reporting(E_ALL & ~E_NOTICE);
+}
+
+require_once "session.php";
+
+
+
+echo "f: ". $userSession->getSessionVal("test");
+
 
 ?>
