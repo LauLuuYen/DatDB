ddcc9e9a8587479c85f0756903a25779dd4e82d6
Akeiten <Akeiten@gmail.com>
2015-02-27 18:03:47 +0000

    Update makeassessment.php

--- php/makeassessment.php
+++ php/makeassessment.php
@@ -1,1 +1,75 @@
+<?php
 
+  header("Access-Control-Allow-Origin: *");
+  define("DEBUG", true);
+  if (DEBUG) {
+      ini_set("display_errors",1);
+      ini_set("display_startup_errors",1);
+      error_reporting(E_ALL & ~E_NOTICE);
+  }
+  include "include/config.php";
+  /*
+  *   Echoes to the client the result of the process.
+  *   @params: bool - $success, string - $msg
+  *   @return: none
+  */
+  function result($success, $msg) {
+      $response["success"] = $success;
+      $response["message"] = $msg;
+      echo json_encode($response);
+  }
+
+class Assessment{
+
+  public function __construct($groupID, $reportID, $feedback, $score, $userID)
+  {
+    $this->groupID = $groupID;
+    $this->reportID = $reportID;
+    $this->feedback = $feedback;
+    $this->score = $score;
+    $this->userID = $userID;
+  }   
+  
+  public function checkInputs()
+  {
+    
+    if(!is_int($this->groupID))
+    {
+      result(false, "groupID must be set");
+      
+    }
+    else if(!is_int($this->reportID))
+    {
+      result(false, "reportID must be set");
+    }
+    else if(strlen($this->feedback) === 0)
+    {
+      result(false, "feedback must be set");
+    }
+    else if(!is_int($this->score))
+    {
+      result(false, "score must be set");
+    }
+    else if(!is_int($this->userID))
+    {
+      result(false, "userID must be set");
+    }
+    else
+    {
+      return true;
+    }
+    return false;
+  }
+  
+}
+
+  $groupID = 21;
+  $reportID = 2071;
+  $feedback = "Princess Latifa";
+  $score = 4;
+  $userID = 51;
+  
+  
+  
+  
+?>
