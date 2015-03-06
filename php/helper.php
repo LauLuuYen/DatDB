800ef1eb89186c9b10987d11e92bedb479090718
Tuan <tuan_v_nguyen@hotmail.com>
2015-03-06 17:26:43 +0000

    helper1

--- php/helper.php
+++ php/helper.php
@@ -1,7 +1,12 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-
+define("DEBUG", true);
+if (DEBUG) {
+    ini_set("display_errors",1);
+    ini_set("display_startup_errors",1);
+    error_reporting(E_ALL & ~E_NOTICE);
+}
 
 class Helper {
 
