9df4c04eb914b51ff473df6909ba1c87f313347b
Akeiten <Akeiten@gmail.com>
2015-03-06 17:59:35 +0000

    Update helper.php

--- php/helper.php
+++ php/helper.php
@@ -3,24 +3,53 @@
 header("Access-Control-Allow-Origin: *");
 
 
-function getForumID($conn, $id) {
+function getForumID($conn, $userid) {
     $stmt = $conn->prepare("SELECT id FROM forum WHERE groupid=(SELECT groupid FROM users WHERE id=?);");
-    $stmt->bind_param("i", $id);
+    $stmt->bind_param("i", $userid);
 
     if ($stmt->execute()) {
         $stmt->store_result();
-        $stmt->bind_result($userID);
+        $stmt->bind_result($forumID);
         
         $registrant = $stmt->fetch();//Bind result with row
         $stmt->close();
 
-        return $userID;
+        return $forumID;
         
     } else {
         die("An error occurred performing a request");
     }
 }
 
+function getAllThreads($conn, $forumID)
+{
+    $stmt = $conn->prepare("SELECT id,title,timestamp FROM thread WHERE forumid=?");
+    $stmt->bind_param("i", $forumID);
+    
+     if ($stmt->execute()) {
+        $stmt->store_result();
+        $stmt->bind_result($id,$title,$timestamp);
+        
+	   	$data = array();
+	   	while($stmt->fetch())
+	   	{
+	   	    $row = array();
+	   	    $row["threadID"] = $id;
+	   	    $row["title"] = $title;
+	   	    $row["timestamp"] = $timestamp;
+	   	    $data[] = $row;
+	   	   
+	   	}
+        $stmt->free_result();
+        $stmt->close();
+
+        return $data;
+        
+    } else {
+        die("An error occurred performing a request");
+    }
+    
+}
 
 
 ?>
