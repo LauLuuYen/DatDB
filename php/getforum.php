67cf57abb564042ff11330d5af5b408cc9d4582a
Akeiten <Akeiten@gmail.com>
2015-03-06 17:11:29 +0000

    Update getforum.php

--- php/getforum.php
+++ php/getforum.php
@@ -6,6 +6,11 @@ if (DEBUG) {
     ini_set("display_startup_errors",1);
     error_reporting(E_ALL & ~E_NOTICE);
 }
+
+require_once("include/config.php");
+require_once("helper.php");
+require_once("session.php");
+
 /*
 *   Echoes to the client the result of the process.
 *   @params: bool - $success, string - $msg
@@ -21,25 +26,42 @@ class Forum
 {
   public function __construct()
   {
+    $this->userID = $_SESSION["userID"];
+    $this->name = $_SESSION["name"];
+    $this->lastname = $_SESSION["lastname"];
+    $this->roleID = $_SESSION["roleID"];
+    $this->groupID = $_SESSION["groupID"];
     
   }
   
+  public function checkInputs()
+  {
+      //TODO check role
+      return true;
+  }
   
+  public function retrieve()
+  {
+      $this->conn = connectDB();
+      $forumID = $helper->getForumID($this->conn, $this->userID);
+      echo $forumID;
+  }
   
   
 }
 
-require_once("session.php");
 
-    if($userSession->isLoggedIn())
-    {
-        echo "Logged in";
-    }
-    else
-    {
-        result(false, "Not logged in");
-        
-    }
+
+if($userSession->isLoggedIn())
+{
+    echo "Logged in";
+    $forum = new Forum();
+}
+else
+{
+    result(false, "Not logged in");
+    
+}
 
 
 ?>
