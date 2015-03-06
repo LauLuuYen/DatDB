ca3210397a890622dcc1c2fab397aaabb79ac75d
Tuan <tuan_v_nguyen@hotmail.com>
2015-03-06 17:28:01 +0000

    helper2

--- php/getforum.php
+++ php/getforum.php
@@ -8,6 +8,7 @@ if (DEBUG) {
 }
 
 require_once("include/config.php");
+require_once("helper.php");
 require_once("session.php");
 
 /*
@@ -43,8 +44,6 @@ class Forum
   {
       $this->conn = connectDB();
       
-      require_once "helper.php";
-
       $forumID = $helper->getForumID($this->conn, $this->userID);
       echo $forumID;
       closeDB($this->conn);
