8fec4c987cd293a890d7ecf41ba81bc7eb45ac0f
Tuan <tuan_v_nguyen@hotmail.com>
2015-03-06 17:32:29 +0000

    helper2

--- php/helper.php
+++ php/helper.php
@@ -3,26 +3,22 @@
 header("Access-Control-Allow-Origin: *");
 
 
-function getForumID($conn, $id)
-	{
-		$stmt = $conn->prepare("SELECT id FROM forum WHERE groupid=(SELECT groupid FROM users WHERE id=?);");
-        $stmt->bind_param("i", $id);
+function getForumID($conn, $id) {
+    $stmt = $conn->prepare("SELECT id FROM forum WHERE groupid=(SELECT groupid FROM users WHERE id=?);");
+    $stmt->bind_param("i", $id);
 
-        if ($stmt->execute()) {
-            $stmt->store_result();
-            $stmt->bind_result($userID);
-            
-            $registrant = $stmt->fetch();//Bind result with row
-            $stmt->close();
+    if ($stmt->execute()) {
+        $stmt->store_result();
+        $stmt->bind_result($userID);
+        
+        $registrant = $stmt->fetch();//Bind result with row
+        $stmt->close();
 
-            return $userID;
-            
-        } else {
-            die("An error occurred performing a request");
-        }
-	}
-    
-    
+        return $userID;
+        
+    } else {
+        die("An error occurred performing a request");
+    }
 }
 
 
