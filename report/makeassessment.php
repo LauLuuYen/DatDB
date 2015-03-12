55fee6a1413a3c25e733e6b754e5d001b51915bf
Akeiten <Akeiten@gmail.com>
2015-03-12 17:51:05 +0000

    Update makeassessment.php

--- report/makeassessment.php
+++ report/makeassessment.php
@@ -30,7 +30,7 @@
     -->
     
     <div class="input_wrapper">
-        <textarea cols="95" rows="3" class="input_text content" ng-model="assessment.feedback" ng-change="onChange('feedback_e')" placeholder="Assessment feedback" maxlength="500"></textarea>        
+        <textarea cols="95" rows="3" class="input_text content" style="width: 80%" ng-model="assessment.feedback" ng-change="onChange('feedback_e')" placeholder="Assessment feedback" maxlength="500"></textarea>        
         <div id="feedback_e" class="error invisible">Error: Please type in a assessment feedback</div>
         <br>
     </div>
