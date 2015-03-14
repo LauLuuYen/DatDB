3b9285fded4b8c4fb703976c6bdee08ba138a3db
Tuan <tuan_v_nguyen@hotmail.com>
2015-03-14 16:23:35 +0000

    get all for admin

---
+++ php/getallforadmin.php
@@ -0,0 +1,46 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+
+class AdminInfo {
+
+    public function __construct() {
+        session_start();
+        $this->userID = $_SESSION["userID"];
+        $this->name = $_SESSION["name"];
+        $this->lastname = $_SESSION["lastname"];
+        $this->roleID = $_SESSION["roleID"];
+        $this->groupID = $_SESSION["groupID"];
+    }
+	
+    
+    public function retrieve() {
+        
+        require_once "include/sql_helper.php";
+        $this->sql_helper = new SQL_Helper();
+        
+        $data = array();
+        $profile = array();
+        $profile["userID"] = $this->userID;
+        $profile["name"] = $this->name;
+        $profile["lastname"] = $this->lastname;
+        $profile["groupID"] = $this->groupID;
+        
+        $data["profile"] = $profile;
+        
+
+        $data["assignments"] = $this->sql_helper->getAllAssignments();
+
+        $this->sql_helper->close();
+        
+        return $data;
+    }
+    
+}
+
+//TODO check role
+$adminInfo = new AdminInfo();
+echo $adminInfo->retrieve();
+
+?>
