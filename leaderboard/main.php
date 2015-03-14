f618adb2d5e50f485aac74c4aca0882459ed9086
Akeiten <Akeiten@gmail.com>
2015-03-14 19:34:58 +0000

    Update main.php

--- leaderboard/main.php
+++ leaderboard/main.php
@@ -5,8 +5,6 @@
         function createLeaderboard() 
         {
             //Build an array containing assignment records.
-            var jsLeaderBoardArray = <?php echo json_encode($leaderBoardDataArray) ?>;
-            alert(JSON.parse(jsLeaderBoardArray));
             var assigment = new Array();
             assigment.push(["Rank", "Group Name", "Average mark"]);
             assigment.push([1, "Parishilton", "4.5"]);
@@ -80,6 +78,12 @@ class LeaderboardClass
 
 <div id="dvTable"></div>
 
+<script type="text/javascript">
+
+            var jsLeaderBoardArray = <?php echo json_encode($leaderBoardDataArray) ?>;
+            alert(JSON.parse(jsLeaderBoardArray));
+
+</script>
 
 </body>
 </html>
