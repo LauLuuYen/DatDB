1bcd230ee9d397cf33e1e6a7877c6192204a6a3f
Akeiten <Akeiten@gmail.com>
2015-03-15 01:41:02 +0000

    Update main.php

--- leaderboard/main.php
+++ leaderboard/main.php
@@ -1,50 +1,7 @@
 <html>
 <head>
 
-    <script type="text/javascript">
-        function createLeaderboard() 
-        {
-            //Build an array containing assignment records.
-            var jsLeaderboardDataJSON = <?php echo json_encode($leaderBoardDataArray); ?>;
-            var assigment = new Array();
-            assigment.push(["Rank", "Group Name", "Average mark"]);
-            for (i in jsLeaderboardDataJSON)
-		{	
-			assigment.push([1, "Parishilton", "4.5"]);
-			//assigment.push([jsLeaderboardDataJSON[i].groupName, jsLeaderboardDataJSON[i].groupID, jsLeaderboardDataJSON[i].averageMark]);;
-		}
-         
-            //Create a HTML Table element.
-            var table = document.createElement("TABLE");
-            table.border = "1";
-         
-            //Get the count of columns.
-            var columnCount = assigment[0].length;
-         
-            //Add the header row.
-            var row = table.insertRow(-1);
-            for (var i = 0; i < columnCount; i++) {
-                var headerCell = document.createElement("TH");
-                headerCell.innerHTML = assigment[0][i];
-                row.appendChild(headerCell);
-            }
-         
-            //Add the data rows.
-            for (var i = 1; i < assigment.length; i++) 
-            {
-                row = table.insertRow(-1);
-                for (var j = 0; j < columnCount; j++) 
-                {
-                    var cell = row.insertCell(-1);
-                    cell.innerHTML = assigment[i][j];
-                }
-            }   
-     
-            var dvTable = document.getElementById("dvTable");
-            dvTable.innerHTML = "";
-            dvTable.appendChild(table);
-        }
-    </script>
+
 <style>
 h1 {
     border-bottom: 3px solid #cc9900;
@@ -131,6 +88,51 @@ class LeaderboardClass
 	//	 }
     	//	alert(js_array);
 	</script>
+	
+	    <script type="text/javascript">
+        function createLeaderboard() 
+        {
+            //Build an array containing assignment records.
+            var jsLeaderboardDataJSON = <?php echo json_encode($leaderBoardDataArray); ?>;
+            var assigment = new Array();
+            assigment.push(["Rank", "Group Name", "Average mark"]);
+            for (i in jsLeaderboardDataJSON)
+		{	
+			assigment.push([1, "Parishilton", "4.5"]);
+			//assigment.push([jsLeaderboardDataJSON[i].groupName, jsLeaderboardDataJSON[i].groupID, jsLeaderboardDataJSON[i].averageMark]);;
+		}
+         
+            //Create a HTML Table element.
+            var table = document.createElement("TABLE");
+            table.border = "1";
+         
+            //Get the count of columns.
+            var columnCount = assigment[0].length;
+         
+            //Add the header row.
+            var row = table.insertRow(-1);
+            for (var i = 0; i < columnCount; i++) {
+                var headerCell = document.createElement("TH");
+                headerCell.innerHTML = assigment[0][i];
+                row.appendChild(headerCell);
+            }
+         
+            //Add the data rows.
+            for (var i = 1; i < assigment.length; i++) 
+            {
+                row = table.insertRow(-1);
+                for (var j = 0; j < columnCount; j++) 
+                {
+                    var cell = row.insertCell(-1);
+                    cell.innerHTML = assigment[i][j];
+                }
+            }   
+     
+            var dvTable = document.getElementById("dvTable");
+            dvTable.innerHTML = "";
+            dvTable.appendChild(table);
+        }
+    </script>
 	
 
 <div id="dvTable"></div>
