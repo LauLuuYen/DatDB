e75f05603ea409b169873f8037ff0a05d6aa7a2b
Tuan <tuan_v_nguyen@hotmail.com>
2015-03-16 11:52:45 +0000

    dgsf

--- php/get_assessmentmarks.php
+++ php/get_assessmentmarks.php
@@ -19,7 +19,7 @@ class Get {
         $data = array();
         
         //Get all assessment marks
-        $data["assessments"] = $this->sql_helper->getAssessmentMarks($this->groupID);
+        $data["assessments"] = $this->sql_helper->getAssessmentMarksInGroup($this->groupID);
         
         $this->sql_helper->close();
         
