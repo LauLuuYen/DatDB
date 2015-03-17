ecd114b7b4636ddf412a78d9ebb7cd1f8b1d194b
Akeiten <Akeiten@gmail.com>
2015-03-17 21:49:40 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -51,7 +51,8 @@ app.controller('Login', function ($scope, master) {
                 },
 
                 error: function(xhr, status, error) {
-                console.log(JSON.stringify(xhr));
+                	
+                console.log("Testing"+JSON.stringify(xhr));
                     hideLoading();
                     alert("An error occurred.  Please try again in a few moments.");
                 }
