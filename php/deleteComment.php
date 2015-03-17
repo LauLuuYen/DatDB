23b3a819d81ec4c20d5e9c5678745d73247c9c97
Tuan <tuan_v_nguyen@hotmail.com>
2015-03-17 17:28:35 +0000

    jksbfgjk

--- php/deleteComment.php
+++ php/deleteComment.php
@@ -1,54 +1,65 @@
 <?php
 
-  class commentEraser
-  {
+class commentEraser
+{
     public function __construct($commentID) {
       
     $this->userID = $_SESSION["userID"];
-    $this->commentID = $commentID;
+    $this->commentID = (int)$commentID;
     }
   
+    public function checkInputs() {
+        if (is_null($this->commentID) || $this->commentID == 0) {
+            result(false, "Invalid commentID");
+        } else {
+            return true;
+        }
+        return false;
+    }
+    
   	public function deleteComment()
   	{
-  		require_once "include/sql_helper.php";
-          $this->sql_helper = new SQL_Helper();
-          //echo "a";
-          //echo $this->userID;
-          $success = $this->sql_helper->deleteComment($this->commentID, $this->userID);
-          //echo "test";
-          $this->sql_helper->close();
-          $url = "http://" . $_SERVER["HTTP_HOST"]."/forum";
-          header("Location: " . $url);
+        require_once "include/sql_helper.php";
+        $this->sql_helper = new SQL_Helper();
+
+        $success = $this->sql_helper->deleteComment($this->commentID, $this->userID);
+
+        $this->sql_helper->close();
+        
+        $url = "http://" . $_SERVER["HTTP_HOST"]."/forum";
+        header("Location: " . $url);
   	}
   	
-  }
+}
   
-    require_once "session.php";
-    if($userSession->isLoggedIn("student")) 
-    {
-      $commentID = 171;
-      $commentEraser = new commentEraser($commentID);
-      $commentEraser->deleteComment();
-      
-    /*  if(!empty($_POST)) 
-      {
-          $threadID = $_POST["threadID"];
-          $input = $_POST["comment"];
-          $comment = new Comment($input, $threadID);
-          if($comment->checkInputs()) 
-          {
-            $comment->makeComment();
-          }
-      } 
-      else 
-      {
-        result(false, "Error in request!");
-      }*/
-    } 
-    else 
-    {
-    result(false, "Session timeout");
+require_once "session.php";
+
+if($userSession->isLoggedIn("student")) 
+{
+    $commentID = 241;
+    $commentEraser = new commentEraser($commentID);
+
+    if ($commentEraser->checkInputs()) {
+        $commentEraser->deleteComment();
     }
+      
+    if(!empty($_POST)) {
+        $commentID = $_POST["commentID"];
+        $commentEraser = new commentEraser($commentID);
+
+        if ($commentEraser->checkInputs()) {
+            $commentEraser->deleteComment();
+        }
+    }
+    else
+    {
+        result(false, "Error in request!");
+    }
+}
+else
+{
+    result(false, "Session timeout");
+}
 
 
 
