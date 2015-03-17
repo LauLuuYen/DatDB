14029e27c9ea7ba2fb22376903af6d33bbc53d0c
Akeiten <Akeiten@gmail.com>
2015-03-17 18:48:02 +0000

    Update test.php

--- php/test.php
+++ php/test.php
@@ -3,6 +3,7 @@
 session_start();
 echo "Hello Now";
 require_once "include/sql_helper.php";
+echo "Hello Pikachu";
 $this->sql_helper = new SQL_Helper();
 echo "Hello";
 $name = $this->sql_helper->getRole($_SESSION["roleID"]);
