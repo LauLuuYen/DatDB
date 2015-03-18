14a75f9fb7e077477ebfedf7f22cf15578220031
Akeiten <Akeiten@gmail.com>
2015-03-18 17:15:52 +0000

    Update viewassessment.php

--- report/viewassessment.php
+++ report/viewassessment.php
@@ -3,14 +3,14 @@
     <div class="linebreak"></div>
     <div>Here is the assessment of your report made by the group.</div><br>
 
-    <div class="heading2">By Group: {{group.name}}</div>
+    <div class="heading2">Your report was marked by: {{group.name}}</div>
     <div style="width:300px; height:35px; background-color:lightgray; font-size:18px;">&nbsp;Their Aggregrated Mark: {{group.score}}/5</div><br>
 
 
-    <div class="heading2">Assessed on: {{assessment.timestamp}}</div>
+    <div class="heading2">{{group.name}} assessed your report on: {{assessment.timestamp}}</div>
     
     <div class="input_wrapper">
-        <div class="heading2">Your Report Mark:</div>
+        <div class="heading2">{{group.name}} gave your reprot the mark:</div>
         <span>
             <span ng-repeat="star in stars" class="viewstar" ng-class="star.state"></span>
         </span>
@@ -18,7 +18,7 @@
     </div>
     
     <div class="input_wrapper">
-        <div class="heading2">Your Report Feedback:</div>
+        <div class="heading2">{{group.name}} gave following feedback:</div>
         <div class="feedback"></div>
     </div>
 
