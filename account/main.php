90ecfdb3eb783476f6896868445e649d2529cb7c
Tuan <tuan_v_nguyen@hotmail.com>
2015-03-18 11:57:53 +0000

    sgsfg

--- account/main.php
+++ account/main.php
@@ -1,6 +1,7 @@
-<div>
-    Accountpage
-
+<div class="main">
+    <div class="heading">Account</div>
+    <div class="linebreak"></div>
+    <div>Here you can change your password.</div>
 
 
 </div>
