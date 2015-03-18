6bf5c400190ddf9e16c3ff643dcbfc08b3dad9e7
Rex <lauyiuyeung2@yahoo.com.hk>
2015-03-18 16:49:14 +0000

    Update main.php

--- about/main.php
+++ about/main.php
@@ -4,11 +4,11 @@
     <div class="heading2">Welcome to our database project website for COMP3013</div> </br>
 
     <div class="heading3">About the Course</div>
-    <div>
+    <div class="aboutbody">
         This course is taught by two professors, John Dowell and Graham Roberts. It covers advanced data modelling and database development methodology, the techniques exploited by relational database technologies relating in particular to query processing and transaction management, and post relational database technologies including object oriented databases and web databases.
     </div>
     <div class="heading3">About the Project</div>
-    <div>
+    <div class="aboutbody">
         In this project, we are focus on a scenario in which a college-level class studying consumer informatics is divided into twenty groups of three
 students to each produce a report surveying reviews of the new LG G Flex 2 smartphone. We will need to develop a learning environment for this scenario. The learning environment is a web facing database system accessed through a browser. Each group will have its own profile and space on the system. Through this space the group will provide access to its own report and obtain access to the reports of other
 groups; upload or submit its assessments of other groups’ reports; receive assessments of its own report from other groups. 
@@ -17,7 +17,7 @@ groups; upload or submit its assessments of other groups’ reports; receive ass
     <div>
         <img src="/img/group.jpg" alt="group photo">
     </div>
-    <div>
+    <div class="aboutbody">
         Rex Lau: zcabyyl@ucl.ac.uk </br>
         Tan Luu: Insert Email </br>
         Tuan Nguyen: zcabtng@ucl.ac.uk </br>
