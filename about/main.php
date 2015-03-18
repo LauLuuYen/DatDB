3cc2fb731ba30af014329113af6f76f775893800
Rex <lauyiuyeung2@yahoo.com.hk>
2015-03-18 14:54:37 +0000

    Update main.php

--- about/main.php
+++ about/main.php
@@ -8,6 +8,9 @@
         This course is taught by two professors, John Dowell and Graham Roberts. It covers advanced data modelling and database development methodology, the techniques exploited by relational database technologies relating in particular to query processing and transaction management, and post relational database technologies including object oriented databases and web databases.
     </div>
     <div class="heading3">About the Project</div>
+    <div>
+        
+    </div>
     <div class="heading3">About Us</div>
     <div>
         Rex Lau: zcabyyl@ucl.ac.uk </br>
