71e70c4a12398196942463e605ba4dd329bf6e0d
Akeiten <Akeiten@gmail.com>
2015-03-18 18:11:22 +0000

    Update leaderboard.php

--- admin/leaderboard.php
+++ admin/leaderboard.php
@@ -133,12 +133,12 @@ table tr:nth-child(even) {
             for (var k = 1; k < leadingboardArray.length; k++)
            {
             	//alert("LOL");
-            	if(leadingboardArray[k][1] == "Zeldafans")
+            	//if(leadingboardArray[k][1] == "Zeldafans")
             	//{
             		//alert("LOL");
             		//console.log("Your group name is: " + leadingboardArray[k][1]);
             		alert("Your group name is: " + leadingboardArray[k][1]);
-            	}
+            	//}
             }
             
 
