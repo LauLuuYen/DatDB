2335b9ef9e3f0b91e5bde5c67a43ce2708cc8a17
Tuan <tuan_v_nguyen@hotmail.com>
2015-03-18 05:36:51 +0000

    sfg

--- about/main.php
+++ about/main.php
@@ -1,7 +1,12 @@
 <div class="about">
     <div class="heading">About</div>
     <div class="linebreak"></div>
+    <div>Welcome to our database project website for COMP3001</div>
 
+    <div>
+        Rex Lau: Insert Email
+        Tan Luu: Insert Email
+        Tuan Nguyen: zcabtng@ucl.ac.uk
 
-
+    </div>
 </div>
