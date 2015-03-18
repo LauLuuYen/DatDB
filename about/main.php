375b9199b5675d57d4e35e47b7108add37ff0625
Rex <lauyiuyeung2@yahoo.com.hk>
2015-03-18 14:13:13 +0000

    Update main.php

--- about/main.php
+++ about/main.php
@@ -3,6 +3,8 @@
     <div class="linebreak"></div>
     <div class="heading2">Welcome to our database project website for COMP3013</div>
 
+    <div class="heading3">About the Course</div>
+    <div class="heading3">About the Project</div>
     <div class="heading3">About Us</div>
     <div>
         Rex Lau: zcabyyl@ucl.ac.uk 
