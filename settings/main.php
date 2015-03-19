f6b22cec9440408b73959d5849cc0326c44827ac
Rex <lauyiuyeung2@yahoo.com.hk>
2015-03-19 13:42:45 +0000

    Update main.php

--- settings/main.php
+++ settings/main.php
@@ -18,7 +18,7 @@
                 ng-change="onChange('e2')"></input>
             <div id="e2" class="error invisible">Error: Please enter your new password</div>
             
-            <div class="heading2">Old password:</div>
+            <div class="heading2">Confirm password:</div>
             <input type="password" class="input_text" ng-model="pw.p2" maxlength=40
                 ng-change="onChange('e3')"></input>
             <div id="e3" class="error invisible">Error: Passwords do not match</div>
