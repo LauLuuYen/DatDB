e9a840d1675164e394ac6f10b5c43a48633d1131
Akeiten <Akeiten@gmail.com>
2015-03-19 14:52:47 +0000

    Update all.php

--- admin/all.php
+++ admin/all.php
@@ -63,7 +63,7 @@
 
 
     <br>
-    <div class="heading2">All Reports</div>
+    <div class="heading2" style="table-layout:fixed; width:100%; overflow:hidden; border:1px solid #f00; word-wrap:break-word;">All Reports</div>
     <table class="randomtable">
         <tr class="head">
             <td>ID</td>
