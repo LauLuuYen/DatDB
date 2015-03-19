88c05ea0c7e4c7bc8ab57c954c3ae2e52fca33e9
Tuan <tuan_v_nguyen@hotmail.com>
2015-03-19 21:31:02 +0000

    groupid

--- php/include/sql_helper.php
+++ php/include/sql_helper.php
@@ -966,7 +966,7 @@ class SQL_Helper {
 	//Leaderboard function
 	public function fetchLeaderBoard()
 	{
-		$stmt = $this->conn->prepare("SELECT groups.name,reports.groupid, reportid, AVG(score) FROM assessments INNER JOIN reports ON (assessments.reportid = reports.id) INNER JOIN groups ON (reports.groupid = groups.id) GROUP BY reportid ORDER BY AVG(score) DESC;");
+		$stmt = $this->conn->prepare("SELECT groups.name,reports.groupid, reportid, AVG(score) FROM assessments INNER JOIN reports ON (assessments.reportid = reports.id) INNER JOIN groups ON (reports.groupid = groups.id) GROUP BY groupid ORDER BY AVG(score) DESC;");
 		if ($stmt->execute()) 
 		{
 	            $stmt->store_result();
